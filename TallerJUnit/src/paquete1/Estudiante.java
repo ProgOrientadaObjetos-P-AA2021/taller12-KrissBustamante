57285b7afdba1761ca76277c0e54cdd8b88d6c7b
KrissBustamante <krissbustamante25@gmail.com>
2021-07-08 17:31:40 -0500

    Trabajo

--- TallerJUnit/src/paquete1/Estudiante.java
+++ TallerJUnit/src/paquete1/Estudiante.java
@@ -1,6 +1,8 @@
 
 package paquete1;
 
+import java.util.ArrayList;
+
 public class Estudiante {
     // 1.  Declarar
     // se declaran datos o atributos con visibilidad protegido
@@ -13,5 +15,31 @@ public class Estudiante {
     // # edadEstudiante: Entero
     protected int edadEstudiante;
     
-
+public void establecerNombresEstudiante(String n){
+        nombresEstudiante = n;
+    
+    }
+    
+    public String obtenerNombresEstudiante(){
+        return nombresEstudiante;
+    
+    }
+   public void establecerApellidosEstudiante(String n){
+        apellidosEstudiante = n;
+    
+    }
+    
+    public String obtenerApellidosEstudiante(){
+        return apellidosEstudiante;
+    
+    }
+    public void establecerIdentificacionEstudiante(String n){
+        identificacionEstudiante = n;
+    
+    }
+    
+    public String obtenerIdentificacionEstudiante(){
+        return identificacionEstudiante;
+    
+    }
 }
--- TallerJUnit/src/paquete1/EstudianteDistancia.java
+++ TallerJUnit/src/paquete1/EstudianteDistancia.java
@@ -15,14 +15,68 @@ public class EstudianteDistancia extends Estudiante{
     
     }
     
-    public void obtenerNotas(ArrayList<Nota> ns){
-        notas = ns;
+    public ArrayList<Nota> obtenerNotas(){
+        return notas;
+    
+    }
+
+     public void establecerMejorNota(){
+       for (int i = 0; i < notas.size(); i++) {
+            if (notas.get(i).obtenerValor() > mejorNota) {
+                mejorNota = notas.get(i).obtenerValor();
+            }
+        }
     
     }
     
+    public double obtenerMejorNota(){
+        return mejorNota;
+    
+    }
+    public void establecerPeorNota() {
+        double s = 10;
+        for (int i = 0; i < notas.size(); i++) {
+            if (s > notas.get(i).obtenerValor()) {
+                s = notas.get(i).obtenerValor();
+            }
+        }
+        peorNota = s;
+    }
+       
+     
+    public double obtenerPeorNota(){
+      
+    return peorNota;
+    }
+     public void establecerPromedio(){
+         double suma = 0;
+       for (int i = 0; i < notas.size(); i++) {
+           
+            suma = suma +notas.get(i).obtenerValor() ;
+               
+            }
+       promedio = suma/notas.size();
+    
+    }
+     
+    public double obtenerPromedio(){
+        return promedio; 
+    
+    }
+      
+     
+    
     @Override
     public String toString(){
-        return "";
+        String cadena = String.format("Nombre Estudiante:%s %s\n"
+                 + "Cédula: %s"
+                + "Notas: \n"
+                + "Promedio: %.1f"
+                + "Mejor Nota: %.1f"
+                + "Peor Nota: %f", obtenerNombresEstudiante(),
+                obtenerApellidosEstudiante(),obtenerIdentificacionEstudiante(),
+                obtenerMejorNota(),obtenerPeorNota());
+        return cadena;
     }
     
     
